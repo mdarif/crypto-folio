49ae69d5730a7665ca86a70ef6f250d5f04094a0
Mohammad Arif <arif_iq@yahoo.co.in>
2018-05-04 13:22:14 +0530

    Update App.js
    Cleanup activity.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <!--Start making component from here-->
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Koinex Folio</h1>
@@ -13,9 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <!--End making component from here-->
       </div>
     );
   }
 }
 
-export default App;
+export default App
