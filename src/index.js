deb0626bf1899d2ef9036601c0c286c63acd08f2
Mohammad Arif <arifmd@Mohammads-MacBook-Air.local>
2018-05-04 21:18:53 +0530

    Added the basic material-ui-next table.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import './css/index.css'
+import App from './js/App'
 
-render(
+ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.querySelector('#root')
 )
---
+++ src/js/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '../css/App.css'
+import SimpleTable from '../js/simple-table'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Koinex Ticker</h1>
+        <h2>Add Transactions</h2>
+        <h2>Show Transactions</h2>
+        <h3>Total Portfolio Value</h3>
+        <SimpleTable height="700" width="500" />
+      </div>
+    );
+  }
+}
+
+export default App
