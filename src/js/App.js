d807ab08564730a7427c339c18f01fd681f7611a
Mohammad Arif <arifmd@Mohammads-MacBook-Air.local>
2018-05-10 22:32:45 +0530

    Added plan vannila table to display the data.

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../css/App.css'
 import SimpleTable from '../js/simple-table'
-import FormFields from '../js/add-transaction'
+//import FormFields from '../js/add-transaction'
 
 class App extends Component {
   render() {
@@ -10,11 +10,9 @@ class App extends Component {
         <h1>Koinex Ticker</h1>
         <h2>Add Transactions</h2>
 
-        <FormFields />
-
         <h2>Show Transactions</h2>
         <h3>Total Portfolio Value</h3>
-        <SimpleTable height="700" width="500" />
+        <SimpleTable />
       </div>
     );
   }
