a3e122c3f0d894e52257b60c8c11bff2fde199df
Mohammad Arif <arif.mohammed@gmail.com>
2018-05-11 22:45:26 +0530

    Basic layout is ready now.

--- src/js/App.js
+++ src/js/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react'
 import '../css/App.css'
 import SimpleTable from '../js/simple-table'
 import AddTransaction from '../js/add-transaction'
+import '../css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Koinex Ticker</h1>
+        <h1 className="display-1">Koinex Ticker</h1>
+        {/*
         <h2>Add Transactions</h2>
 
         <h2>Show Transactions</h2>
         <h3>Total Portfolio Value</h3>
+        */}
         <AddTransaction />
-        <SimpleTable />
+        {/*<SimpleTable />*/}
       </div>
     );
   }
--- src/js/add-transaction.js
+++ src/js/add-transaction.js
@@ -3,87 +3,44 @@ import '../css/bootstrap.min.css'
 
 export default class AddTransaction extends React.Component {
     render() {
-        return
-        <form>
+        return <form>
           <div className="form-group row">
-            <label for="example-text-input" className="col-2 col-form-label">Text</label>
-            <div className="col-10">
-              <input className="form-control" type="text" value="Artisanal kale" id="example-text-input" />
+            <label htmlFor="pair-text-input" className="col-3 col-form-label">Currency Pair</label>
+            <div className="col-8">
+              <input className="form-control" type="text" placeholder="XRP" id="pair-text-input" />
             </div>
           </div>
           <div className="form-group row">
-            <label for="example-search-input" className="col-2 col-form-label">Search</label>
-            <div className="col-10">
-              <input className="form-control" type="search" value="How do I shoot web" id="example-search-input" />
+            <label htmlFor="quantity-input" className="col-3 col-form-label">Quantity</label>
+            <div className="col-8">
+              <input className="form-control" type="text" placeholder="50" id="quantity-input" />
             </div>
           </div>
           <div className="form-group row">
-            <label for="example-email-input" className="col-2 col-form-label">Email</label>
-            <div className="col-10">
-              <input className="form-control" type="email" value="bootstrap@example.com" id="example-email-input" />
+            <label htmlFor="price-per-unit-input" className="col-3 col-form-label">Price per unit</label>
+            <div className="col-8">
+              <input className="form-control" type="text" placeholder="" id="price-per-unit-input" />
             </div>
           </div>
           <div className="form-group row">
-            <label for="example-url-input" className="col-2 col-form-label">URL</label>
-            <div className="col-10">
-              <input className="form-control" type="url" value="https://getbootstrap.com" id="example-url-input" />
+            <label htmlFor="amount-without-fee-input" className="col-3 col-form-label">Amount without fee</label>
+            <div className="col-8">
+              <input className="form-control" type="text" placeholder="1900" id="amount-without-fee-input" />
             </div>
           </div>
           <div className="form-group row">
-            <label for="example-tel-input" className="col-2 col-form-label">Telephone</label>
-            <div className="col-10">
-              <input className="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input" />
+            <label htmlFor="fees-input" className="col-3 col-form-label">Fees</label>
+            <div className="col-8">
+              <input className="form-control" type="text" placeholder="1900" id="fees-input" />
             </div>
           </div>
           <div className="form-group row">
-            <label for="example-password-input" className="col-2 col-form-label">Password</label>
-            <div className="col-10">
-              <input className="form-control" type="password" value="hunter2" id="example-password-input" />
+            <label htmlFor="total-inlcuding-fees-input" className="col-3 col-form-label">Total amount including fee</label>
+            <div className="col-8">
+              <input className="form-control" type="number" placeholder="1904.75" id="total-inlcuding-fees-input" />
             </div>
           </div>
-          <div className="form-group row">
-            <label for="example-number-input" className="col-2 col-form-label">Number</label>
-            <div className="col-10">
-              <input className="form-control" type="number" value="42" id="example-number-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-datetime-local-input" className="col-2 col-form-label">Date and time</label>
-            <div className="col-10">
-              <input className="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-date-input" className="col-2 col-form-label">Date</label>
-            <div className="col-10">
-              <input className="form-control" type="date" value="2011-08-19" id="example-date-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-month-input" className="col-2 col-form-label">Month</label>
-            <div className="col-10">
-              <input className="form-control" type="month" value="2011-08" id="example-month-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-week-input" className="col-2 col-form-label">Week</label>
-            <div className="col-10">
-              <input className="form-control" type="week" value="2011-W33" id="example-week-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-time-input" className="col-2 col-form-label">Time</label>
-            <div className="col-10">
-              <input className="form-control" type="time" value="13:45:00" id="example-time-input" />
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="example-color-input" className="col-2 col-form-label">Color</label>
-            <div className="col-10">
-              <input className="form-control" type="color" value="#563d7c" id="example-color-input" />
-            </div>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary btn-lg btn-block">Submit</button>
         </form>
     }
 }
